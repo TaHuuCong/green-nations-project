b666a5bd249d005aabf0294c8b91817e74b94c3e
TAHUUCONG\Ta Huu Cong <cong.tsunajj310@gmail.com>
2018-10-11 22:55:51 +0700

    ud

--- src/app/country-detail/country-detail.component.ts
+++ src/app/country-detail/country-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../input-data/app.country';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountryService } from '../services/country.service';
+import { FieldDefinition } from '../../fw/dynamic-forms/field-definition';
 
 @Component({
   selector: 'app-country-detail',
@@ -7,9 +11,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryDetailComponent implements OnInit {
 
-  constructor() { }
+  country: Country;
+  countryDefinition: Array<FieldDefinition> = [
+    {
+      key: 'id',
+      type: 'number',
+      isId: true,
+      label: 'Id',
+      required: true
+    },
+    { key: 'name',
+      type: 'string',
+      isId: false,
+      label: 'Country Name',
+      required: true
+    },
+    {
+      key: 'epiIndex',
+      type: 'number',
+      isId: false,
+      label: 'EPI Index',
+      required: true
+    }
+  ];
+  errorMessage: string;
+
+  // tham số thứ 3 trong route: country-detail/:id/:operation
+  operation: string;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private countryService: CountryService) { }
 
   ngOnInit() {
+    this.operation = this.route.snapshot.params['operation'];
+
+    // create new country
+    if (this.operation === 'create') {
+      this.country = { id: 0, name: '', epiIndex: null };
+    } else {
+      this.countryService.getCountry(this.route.snapshot.params['id'])
+        .subscribe((newCountry: Country) => this.country = newCountry);
+    }
+  }
+
+  createCountry(country: Country) {
+    country.id = 0;
+    this.errorMessage = null;
+    this.countryService.createCountry(country).subscribe(
+      c => this.router.navigate(['/authenticated/country-maint']),
+      err => this.errorMessage = 'Error creating country'
+    );
+  }
+
+  updateCountry(country: Country) {
+    this.errorMessage = null;
+    this.countryService.updateCountry(country).subscribe(
+      c => this.router.navigate(['/authenticated/country-maint']),
+      err => this.errorMessage = 'Error updating country'
+    );
   }
 
 }
--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -2,19 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { FrameworkConfigService } from './services/framework-config.service';
 import { ScreenService } from './services/screen.service';
 import { MenuService } from './services/menu.service';
 
-import { FrameworkBodyComponent } from './framework-body/framework-body.component';
-import { ContentComponent } from './content/content.component';
-import { TitleBarComponent } from './title-bar/title-bar.component';
-
 import { ScreenLargeDirective } from './directives/screen-large.directive';
 import { ScreenBelowLargeDirective } from './directives/screen-below-large.directive';
 
+import { FrameworkBodyComponent } from './framework-body/framework-body.component';
+import { ContentComponent } from './content/content.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { StatusBarComponent } from './status-bar/status-bar.component';
 import { MenuComponent } from './menus/menu/menu.component';
@@ -22,6 +21,8 @@ import { MenuItemComponent } from './menus/menu-item/menu-item.component';
 import { PopupMenuComponent } from './menus/popup-menu/popup-menu.component';
 import { SignInComponent } from './users/sign-in/sign-in.component';
 import { RegisterUserComponent } from './users/register-user/register-user.component';
+import { DynamicFieldComponent } from './dynamic-forms/dynamic-field/dynamic-field.component';
+import { DynamicFormComponent } from './dynamic-forms/dynamic-form/dynamic-form.component';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,8 @@ import { RegisterUserComponent } from './users/register-user/register-user.compo
     PopupMenuComponent,
     SignInComponent,
     RegisterUserComponent,
+    DynamicFieldComponent,
+    DynamicFormComponent,
     ScreenLargeDirective,
     ScreenBelowLargeDirective,
   ],
@@ -48,9 +51,11 @@ import { RegisterUserComponent } from './users/register-user/register-user.compo
     RouterModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   exports: [
     FrameworkBodyComponent,
+    DynamicFormComponent,
   ],
 })
 export class FwModule { }
